7a8cf372ae55dd54164b3871670b2a5531c821cf
lauer3912 <lauer3912@gmail.com>
2024-04-06 10:11:10 +0800

    init

--- submit.bing.js
+++ submit.bing.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 
-const indexNowKey = 'ec9c9516ea82433086d979e16e1960e0'; // 替换为你的API密钥
-const siteHostName = 'android-transfer.techidaily.com'; // 替换为你的网站URL
+const indexNowKey = '0bb4f0f477b048f585c63cac1ae7eeed'; // 替换为你的API密钥
+const siteHostName = 'fix-guide.techidaily.com'; // 替换为你的网站URL
 const groupItemCount = 100; // 每组最多个URL
 
 
